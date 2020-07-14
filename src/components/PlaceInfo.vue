1b0588ed2fa45402a77d2bf716b973ffd5979acb
Dima <v3il@ukr.net>
2020-07-14 15:37:27 +0300

    Removed bad place info

--- src/components/PlaceInfo.vue
+++ src/components/PlaceInfo.vue
@@ -14,15 +14,15 @@
             {{ placeInfo.restCost.name }}
         </div>
 
-        <div class="place-info__info-block">
-            <span class="bold-label">{{ translateText('restDurations') }}:</span>
-            {{ placeInfo.restDuration.name }}
-        </div>
+        <!--        <div class="place-info__info-block">-->
+        <!--            <span class="bold-label">{{ translateText('restDurations') }}:</span>-->
+        <!--            {{ placeInfo.restDuration.name }}-->
+        <!--        </div>-->
 
-        <div class="place-info__info-block">
-            <span class="bold-label">{{ translateText('companySizes') }}:</span>
-            {{ placeInfo.companySize.name }}
-        </div>
+        <!--        <div class="place-info__info-block">-->
+        <!--            <span class="bold-label">{{ translateText('companySizes') }}:</span>-->
+        <!--            {{ placeInfo.companySize.name }}-->
+        <!--        </div>-->
 
         <div class="place-info__info-block">
             <span class="bold-label">{{ translateText('restTypes') }}:</span>
